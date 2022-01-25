ff6f284fca9bf8e877ae655263b5e3d8275c22af
Wilmar dos Santos <wilmar.santos@gazin.com.br>
2022-01-25 07:18:21 -0300

    Criação do service

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from 'express';
+import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController {
     handle(request: Request, response: Response) {
+
+        const createUserService = new CreateUserService();
         
         const name = request.body.name;
         const email = request.body.email;
+
+        if (name.length === 0 || email.length === 0) {
+            return response.status(400).json({
+                mensagem: "Preencha todos os campos"
+            });
+        }
         
-        return response.json({
-            mensagem: {
-                name,
-                email
-            }
+        const user = createUserService.execute({name, email});
+        return response.status(201).json({
+            mensagem: `Usuário ${name} criado`
         });
     }
 }
